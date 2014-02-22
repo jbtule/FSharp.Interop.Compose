2175e66ebec207db9e5317eb13793845e4d1fff0
jbtule <jay+code@tuley.name>
2014-02-22 00:06:56 -0600

    Added orderby |> thenby test

--- test/Composable.Linq.fsx
+++ test/Composable.Linq.fsx
@@ -20,4 +20,13 @@ module Enumerable =
     [<Fact>]
     let orderByDescending () =
         seq { 'a' .. 'c' } |> Enumerable.orderByDescending (fun c -> c) |> Seq.toList |> should equal ['c';'b';'a']
-    
+
+    [<Fact>]
+    let thenBy () =
+        let p1 = (seq { 'a' .. 'c' }, seq { 1 .. 3}) ||> Seq.zip 
+        let p2 = (seq { 'x' .. 'z' }, seq { 1 .. 3}) ||> Seq.zip 
+        Seq.append p1 p2 
+            |> Enumerable.orderByDescending (fun (_,n)->n) 
+            |> Enumerable.thenBy (fun(a,_)->a)
+            |> Seq.toList
+            |> should equal [('c',3);('z',3);('b',2);('y',2);('a',1);('x',1)]
