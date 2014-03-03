85b51895eddd6b1b7a9551249f10ab1013ea3d60
jbtule <jay+code@tuley.name>
2014-03-02 21:51:31 -0600

    more tests

--- test/Composable.System.fsx
+++ test/Composable.System.fsx
@@ -9,35 +9,21 @@ open Composable.System
 module String =
 
     [<Fact>]
-    let padLeft () =
-        "hello" |> String.padLeft 6 |> should equal " hello"
+    let contains () =
+        "hello" |> String.contains "ll" |> should be True
+        "hello" |> String.contains "z" |> should be False
+
 
     [<Fact>]
-    let padRight () =
-        "hello" |> String.padRight 6 |> should equal "hello "
+    let endsWith () =
+        "hello" |> String.endsWith "lo" |> should be True
+        "hello" |> String.endsWith "he" |> should be False
 
     [<Fact>]
-    let trimStart () =
-        " hello " |> String.trimStart [|' '|] |> should equal "hello "
-
-    [<Fact>]
-    let trimEnd () =
-        " hello " |> String.trimEnd [|' '|] |> should equal " hello"
-
-    [<Fact>]
-    let trim () =
-        " hello " |> String.trim |> should equal "hello"
-        
-    [<Fact>]
-    let startsWith () =
-        ["One";"Two";"Three"]
-           |> Seq.filter (String.startsWith "T")
-           |> Seq.toList
-           |> should equal ["Two";"Three"]
-
-    [<Fact>]
-    let join () =
-        ["1";"2";"3"] |> String.join " " |> should equal "1 2 3"
+    let endsWithFull () =
+        "HELLO"
+            |> String.Full.endsWith "lo" true System.Globalization.CultureInfo.InvariantCulture
+            |> should be True
 
     [<Fact>]
     let isNullOrWhitespace () =
@@ -52,3 +38,83 @@ module String =
            |> Seq.filter (not << String.isNullOrEmpty)
            |> Seq.toList
            |> should equal ["1";"2";"  ";"3";"4"]
+
+    [<Fact>]
+    let join () =
+        ["1";"2";"3"] |> String.join " " |> should equal "1 2 3"
+
+    [<Fact>]
+    let padLeft () =
+        "hello" |> String.padLeft 6 |> should equal " hello"
+
+    [<Fact>]
+    let padLeftFull () =
+        "1" |> String.Full.padLeft 3 '0' |> should equal "001"
+
+    [<Fact>]
+    let padRight () =
+        "hello" |> String.padRight 6 |> should equal "hello "
+
+    [<Fact>]
+    let padRightFull () =
+        "1" |> String.Full.padRight 3 '0' |> should equal "100"
+
+    [<Fact>]
+    let replace () =
+        "hello what"
+            |> String.replace "what" "world"
+            |> should equal "hello world"
+
+    [<Fact>]
+    let split () =
+        "one,two,three"
+            |> String.split [|','|]
+            |> Seq.toList
+            |> should equal ["one";"two";"three"]
+
+    [<Fact>]
+    let startsWith () =
+        ["One";"Two";"Three"]
+           |> Seq.filter (String.startsWith "T")
+           |> Seq.toList
+           |> should equal ["Two";"Three"]
+
+    [<Fact>]
+    let startsWithFull () =
+        ["One";"Two";"Three"]
+           |> Seq.filter (String.Full.startsWith "t" true System.Globalization.CultureInfo.InvariantCulture)
+           |> Seq.toList
+           |> should equal ["Two";"Three"]
+
+    [<Fact>]
+    let toLowerFull () =
+        "LOWER" |> String.Full.toLower System.Globalization.CultureInfo.InvariantCulture |> should equal "lower"
+
+    [<Fact>]
+    let toUpperFull () =
+        "upper" |> String.Full.toUpper System.Globalization.CultureInfo.InvariantCulture |> should equal "UPPER"
+
+
+    [<Fact>]
+    let toLower () =
+        "LOWER" |> String.toLower |> should equal "lower"
+
+    [<Fact>]
+    let toUpper () =
+        "upper" |> String.toUpper |> should equal "UPPER"
+
+    [<Fact>]
+    let trim () =
+        " hello " |> String.trim |> should equal "hello"
+
+    [<Fact>]
+    let trimFull () =
+        "00100" |> String.Full.trim [|'0'|] |> should equal "1"
+
+    [<Fact>]
+    let trimStart () =
+        " hello " |> String.trimStart [|' '|] |> should equal "hello "
+
+    [<Fact>]
+    let trimEnd () =
+        " hello " |> String.trimEnd [|' '|] |> should equal " hello"
