82d860854cdd010b90fd4771b143553955f1b432
jbtule <jay+code@tuley.name>
2018-03-20 09:58:56 -0500

    correct signature

--- src/PORTABLE_259/FSharp.Interop.Compose.Collections.Generic/Comparer.fs
+++ src/PORTABLE_259/FSharp.Interop.Compose.Collections.Generic/Comparer.fs
@@ -4,5 +4,5 @@ namespace FSharp.Interop.Compose.Collections.Generic
 /// Corresponding static methods as functions for [`System.Collections.Generic.Comparer`](http://msdn.microsoft.com/en-us/library/system.collections.generic.comparer)
 module Comparer =
 
-    /// Calls [`Create(comparison)`](http://msdn.microsoft.com/en-us/library/system.collections.generic.comparer.create)
-    let inline create<'T> (comparison:System.Comparison<'T>) = System.Collections.Generic.Comparer<'T>.Create(comparison)
+    /// Calls [`Create(System.Comparison<'T>(comparison))`](http://msdn.microsoft.com/en-us/library/system.collections.generic.comparer.create)
+    let inline create<'T> (comparison:'T->'T->System.Int32) = System.Collections.Generic.Comparer<'T>.Create(System.Comparison<'T>(comparison))
